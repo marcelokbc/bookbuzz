1bfce2b568da2f612a09e19a1578b165666704dc
Marcelo Silva <marcelo.cansilva@gmail.com>
2024-05-06 18:38:07 -0300

    Adding I18n and adjusment devise pages

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,12 @@ class Book < ApplicationRecord
   has_one_attached :image
 
   validates :title, :author, :description, :rating, :price, :image, presence: true
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["author", "created_at", "description", "id", "id_value", "price", "rating", "title", "updated_at"]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    ["comments", "image_attachment", "image_blob"]
+  end
 end
